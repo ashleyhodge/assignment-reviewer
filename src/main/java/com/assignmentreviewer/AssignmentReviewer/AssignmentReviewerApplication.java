25c7ccd3c677d402a28ffd6160086a69c5326b1a
ashleyhodge <ashhodge@comcast.net>
2023-03-22 16:20:33 -0400

    initial commit

---
+++ src/main/java/com/assignmentreviewer/AssignmentReviewer/AssignmentReviewerApplication.java
@@ -0,0 +1,14 @@
+package com.assignmentreviewer.AssignmentReviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentReviewerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AssignmentReviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignmentreviewer/AssignmentReviewer/AssignmentReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignmentreviewer.AssignmentReviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
